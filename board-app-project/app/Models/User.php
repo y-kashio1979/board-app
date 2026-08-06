c02679a059bd4342b49f7360b4b8a32707f72ba4
takahashi <takahashi@exec.co.jp>
2026-08-06 15:36:58 +0900

    Threadsマイグレーションの作成

--- board-app-project/app/Models/User.php
+++ board-app-project/app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function threads()
+    {
+        return $this->hasMany(Threads::class);
+    }
 }
