cb01fb3ee97e67bd27ca77078e8e57cd4a83d73d
takahashi <takahashi@exec.co.jp>
2026-08-06 15:04:18 +0900

    スレッド詳細画面の作成

--- board-app-project/resources/js/router/index.js
+++ board-app-project/resources/js/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Regist from '../users/regist.vue'
 
 import Login from '../users/Login.vue'
+import ThreadShow from '../threads/ThreadShow.vue'
 
 const routes = [
     {
@@ -10,9 +11,14 @@ const routes = [
         component: Login
     },
     {
-       path:'/register',
-       name:'Regist',
-       component: Regist
+        path:'/register',
+        name:'Regist',
+        component: Regist
+    },
+    {
+        path:'/threads/:id',
+        name:'ThreadShow',
+        component: ThreadShow
     }
 ]
 
