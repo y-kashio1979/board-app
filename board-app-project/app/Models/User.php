6a4ad08a6b96e63eee7301a7698b4e2b54c1fc96
takahashi <takahashi@exec.co.jp>
2026-08-06 15:51:18 +0900

    Commentsマイグレーションの作成

--- board-app-project/app/Models/User.php
+++ board-app-project/app/Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Threads::class);
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comments::class);
+    }
 }
