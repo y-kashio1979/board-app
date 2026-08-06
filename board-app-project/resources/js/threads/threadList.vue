a4a6b38c7b75e686e14dff3fb4ff85b3b74cd23d
setoyama <setoyama@exec.co.jp>
2026-08-06 15:46:59 +0900

    スレッド一覧画面の作成

---
+++ board-app-project/resources/js/threads/threadList.vue
@@ -0,0 +1,31 @@
+<script></script>
+
+<template>
+<h2>スレッド一覧</h2>
+<label for="serch">検索</label>
+<input type="text" id="serch">
+<button @click="serch">検索</button>
+<table>
+    <tr>
+        <th>タイトル</th>
+        <th>閲覧数</th>
+        <th>コメント数</th>
+        <th>投稿者</th>
+        <th>投稿日</th>
+    </tr>
+    <tr @click="goDtail">
+        <!--TODO:一覧表示-->
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
+<!--TODO:ページネーション作成-->
+<button>前へ</button>
+<span></span>
+<button>次へ</button>
+</template>
+
+<style></style>
