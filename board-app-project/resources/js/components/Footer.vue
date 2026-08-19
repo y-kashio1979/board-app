6f42fa0cca2cfad1e6c91100c46f017506d94c62
takahashi <takahashi@exec.co.jp>
2026-08-19 10:56:21 +0900

    フッターの作成

---
+++ board-app-project/resources/js/components/Footer.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+    <div>
+        <small> Copyright &copy; 2026 Simple Board Laravel Learning Project </small>
+    </div>
+</template>
+
+<style scoped></style>
