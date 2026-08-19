67441b6d8f3bddd68d4e5fa4458e9c8933d05688
shiraishi <shiraishi@exec.co.jp>
2026-08-19 10:40:18 +0900

    cssの共通スタイルを実装しました

--- board-app-project/resources/js/app.js
+++ board-app-project/resources/js/app.js
@@ -3,5 +3,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
+import '../css/app.css';
 
 createApp(App).use(router).use(createPinia()).mount('#app');
