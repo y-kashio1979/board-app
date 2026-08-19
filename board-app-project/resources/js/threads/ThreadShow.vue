44b0a13f1d56551d0d3e5cdccbc4d0ef8f3c2039
setoyama <setoyama@exec.co.jp>
2026-08-19 11:24:17 +0900

    ルータークエリに変更

--- board-app-project/resources/js/threads/ThreadShow.vue
+++ board-app-project/resources/js/threads/ThreadShow.vue
@@ -14,8 +14,6 @@ const router = useRouter();
 const errorMessage = ref(""); // エラーメッセージを格納する変数
 const maxpreviewLength = 50; // コメントのプレビュー表示の最大文字数
 const AuthSotre = useAuthStore();
-const keyword = route.query.keyword;
-const page = route.query.page;
 // TODO : ログイン情報の呼び出し
 
 const fetchThread = async () => {
@@ -51,7 +49,7 @@ const reFetchThread = (isPosted) => {
 };
 
 const goBack = () => {
-    router.push({ name: "ThreadList", query: { keyword, page } });
+    router.push({ name: "ThreadList", query: route.query });
 };
 
 // 日付を日本語形式でフォーマットする関数
