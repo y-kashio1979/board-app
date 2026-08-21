39527fa358041f68eb637712a7dc75fa416c8d76
shiraishi <shiraishi@exec.co.jp>
2026-08-21 11:04:47 +0900

    トップ画面の見出しをpからh3に変更

--- board-app-project/resources/js/Top.vue
+++ board-app-project/resources/js/Top.vue
@@ -15,14 +15,14 @@ const features = [
 
         <div class="card">
             <div class="section-group">
-                <p class="section-title">概要</p>
+                <h3 class="section-title">概要</h3>
                 <p class="text-text">
                     本アプリはLaravelとVueの学習を目的として作成した、掲示板アプリです。
                 </p>
             </div>
 
             <div class="section-group">
-                <p class="section-title">主な機能</p>
+                <h3 class="section-title">主な機能</h3>
                 <ul class="list-disc pl-6">
                     <li v-for="feature in features">
                         {{ feature }}
